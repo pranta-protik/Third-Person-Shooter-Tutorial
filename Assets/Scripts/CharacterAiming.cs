e0814bc12707caa5f6aef1b4df2db81f08a178f0
Pranta Protik <pranta.cseku@gmail.com>
2023-12-08 19:11:05 +0600

    Aiming with multi aim constraint

--- Assets/Scripts/CharacterAiming.cs
+++ Assets/Scripts/CharacterAiming.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 namespace _Project
 {
     public class CharacterAiming : MonoBehaviour
     {
         [SerializeField] private float _turnSpeed = 15f;
+        [SerializeField] private float _aimDuration = 0.18f;
+        [SerializeField] private Rig _aimLayer;
+        
 
         private Camera _mainCamera;
 
@@ -19,6 +23,18 @@ namespace _Project
         {
             var yawCamera = _mainCamera.transform.eulerAngles.y;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, yawCamera, 0f), _turnSpeed * Time.fixedDeltaTime);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(1))
+            {
+                _aimLayer.weight += Time.deltaTime / _aimDuration;
+            }
+            else
+            {
+                _aimLayer.weight -= Time.deltaTime / _aimDuration;
+            }
         }
     }
 }
